b8363d3c3cc4ec4e7ab2b7e753d722b405e8394c
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-09-25 20:44:15 +0800

    aston: enable udfps animations
    Change-Id: I809787a262e7e19a7f8808c8ebed094a94d7ddd9
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
+TARGET_HAS_UDFPS := true
+
 # LiveDisplay
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
 
