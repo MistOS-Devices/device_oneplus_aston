88fa6d7f171cba908f9a6e22d43c3d1faff4507c
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-10-30 13:13:41 +0800

    aston: Opt-out of Livedisplay SE
    SE: Our hbm doesn't sticks, and max brightness with slider is same as hbm anyway
    
    Change-Id: I682a1a03d5794d072965aa39aff3029d069c1789
    co-authored-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -45,6 +45,7 @@ TARGET_HAS_UDFPS := true
 
 # LiveDisplay
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,false)
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
