d714e4a8f7bed485e8c39b5b2a4a53502555fd74
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-11-19 10:51:17 +0800

    aston: rework mist flags
    Change-Id: I57df1f4e88c2980dbd950b55d467119ae66724b2
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -41,8 +41,6 @@ PRODUCT_COPY_FILES += \
 # Fingerprint
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
-TARGET_HAS_UDFPS := true
-
 # LiveDisplay
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
 $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
